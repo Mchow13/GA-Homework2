8f64f009978afa4b469bf152cee1c735e83c97c1
mchow1973 <marvinchow@marvinchow-macbookpro.local>
2015-09-20 11:53:15 -0700

    Initial Commit

---
+++ GA-Homework2Tests/GA_Homework2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GA_Homework2Tests.swift
+//  GA-Homework2Tests
+//
+//  Created by marvinchow on 9/20/15.
+//  Copyright © 2015 marvinchow. All rights reserved.
+//
+
+import XCTest
+@testable import GA_Homework2
+
+class GA_Homework2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
